dfa6d799602468695b5bb574faf485d12341467b
Ryan Ramdehol <rramdeholshift@gmail.com>
2016-12-21 10:16:16 -0500

    added the boiler plate files for movie_app

--- JS/movie_app.js
+++ JS/movie_app.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
 	console.log("test");
+	// MAKE AN API REQUEST for ALL API CALLS 
+	var apiBaseUrl = "https://api.themoviedb.org/3/";
+	var imageBaseUrl = "http://image.tmdb.org/t/p/";
+	var nowPlayingUrl = apiBaseUrl+"/movies/now_playing?api_key="+apiKey;
+
 });
 
