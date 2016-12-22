43f280820592d0b6399731e9c44c4f8de5318f11
Ryan Ramdehol <rramdeholshift@gmail.com>
2016-12-21 19:35:34 -0500

    added modal functionality

--- JS/movie_app.js
+++ JS/movie_app.js
@@ -1,9 +1,40 @@
 $(document).ready(function(){
 	console.log("test");
 	// MAKE AN API REQUEST for ALL API CALLS 
-	var apiBaseUrl = "https://api.themoviedb.org/3/";
+	var apiBaseUrl = "http://api.themoviedb.org/3/";
 	var imageBaseUrl = "http://image.tmdb.org/t/p/";
-	var nowPlayingUrl = apiBaseUrl+"/movies/now_playing?api_key="+apiKey;
+	const nowPlayingUrl = apiBaseUrl+"movie/now_playing?api_key="+apiKey;
+	var np = [];
+	$.getJSON(nowPlayingUrl, function(nowPlayingData){
+		np = nowPlayingData.results;
+		console.log(nowPlayingUrl);
+		console.log(nowPlayingData);
+		var nowPlayingHTML ="";
+		for(let i=0;i<nowPlayingData.results.length;i++){
+			var poster = imageBaseUrl+"w300"+nowPlayingData.results[i].poster_path;
+			nowPlayingHTML += '<button type="button" class="btn btn-primary" movieID='+nowPlayingData.results[i].id+' data-toggle="modal" data-target="#myModal">';
+				nowPlayingHTML += "<div class='col-sm-12'>";
+					nowPlayingHTML+= '<img src= "'+poster+'">';
+					nowPlayingHTML += "<div></div>";
+				nowPlayingHTML += '</div>';
+			nowPlayingHTML += '</button>';
+		}
+		// Populate the movie 
+		$("#movie-grid").html(nowPlayingHTML);
+		
+
+		$('button').click(function(){
+			console.log(this);
+			this.getAttribute("movie-id");
+			// filtered JSON request
+
+		});
+	});
+	// $(data-toggle="modal").click(function(){
+	// 	console.log("It Works")
+	// });
+
 
 });
 
+ 
